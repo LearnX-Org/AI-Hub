0143d336c514f53af1e56e710b5bd3c3d3970beb
Hayden Thai <thaihayden@gmail.com>
2023-09-06 22:33:36 -0700

    fixng lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["import", "react-hooks"],
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "prettier",
-    "plugin:react-hooks/recommended"
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "rules": {
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { "argsIgnorePattern": "^_", "destructuredArrayIgnorePattern": "^_" }
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
     ],
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      { "prefer": "type-imports", "fixStyle": "inline-type-imports" }
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
     ],
-    "import/consistent-type-specifier-style": ["error", "prefer-inline"],
-    "@typescript-eslint/no-confusing-void-expression": "off",
-    "@typescript-eslint/restrict-template-expressions": "off"
-  }
-}
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+  },
+};
